7c606a2994780365349ff7caca4cfa33836d58d1
Ulysse Carion <ulysse.carion@ssoready.com>
2025-04-03 13:41:40 -0700

    format, add release ci

--- tests/context.test.ts
+++ tests/context.test.ts
@@ -2,17 +2,23 @@ import { accessTokenClaims, credentials, organizationId } from "../src";
 
 describe("context", () => {
   it("reads out credentials", () => {
-    const req = { auth: { accessToken: "abc" } }
-    expect(credentials(req as any)).toEqual("abc")
-  })
+    const req = { auth: { accessToken: "abc" } };
+    expect(credentials(req as any)).toEqual("abc");
+  });
 
   it("reads out accessTokenClaims", () => {
-    const req = { auth: { accessTokenClaims: { organization: { id: "123" } } } }
-    expect(accessTokenClaims(req as any)).toEqual({ organization: { id: "123" } })
-  })
+    const req = {
+      auth: { accessTokenClaims: { organization: { id: "123" } } },
+    };
+    expect(accessTokenClaims(req as any)).toEqual({
+      organization: { id: "123" },
+    });
+  });
 
   it("reads out organizationId", () => {
-    const req = { auth: { accessTokenClaims: { organization: { id: "123" } } } }
-    expect(organizationId(req as any)).toEqual("123")
-  })
-})
+    const req = {
+      auth: { accessTokenClaims: { organization: { id: "123" } } },
+    };
+    expect(organizationId(req as any)).toEqual("123");
+  });
+});
